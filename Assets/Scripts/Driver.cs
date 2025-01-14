7e6aef3868175ab262923441905a494703bc2b18
Duy Nam Tong <145951925+Newboybie@users.noreply.github.com>
2025-01-14 10:19:14 +0700

    Add speed change in game
    Increase speed when pick up the boost, decrease when collision with something.

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 2.0f;
-    [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float moveSpeed = 10.0f;
+    [SerializeField] private float basicSpeed = 10.0f;
+    [SerializeField] private float slowSpeed = 5.0f;
+    [SerializeField] private float boostSpeed = 20.0f;
     void Start()
     {
 
@@ -17,5 +21,23 @@ public class Driver : MonoBehaviour
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Translate(0, moveAmount, 0);
         transform.Rotate(0, 0, -steerAmount);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "BoostPoint")
+        {
+            moveSpeed = boostSpeed;
+        }
+
+        if (other.tag == "Customer" || other.tag == "Package")
+        {
+            moveSpeed = basicSpeed;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        moveSpeed = slowSpeed;
     }
 }
